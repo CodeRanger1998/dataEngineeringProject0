8e3d636e3a76b7dda255c19b5d18f409768d077e
Pranil Ingle <ingle.pranil@gmail.com>
2024-02-02 19:33:15 -0500

    first push

---
+++ assignment0/functions.py
@@ -0,0 +1,59 @@
+import urllib.request
+import io  
+from PyPDF2 import PdfReader
+import re
+import sqlite3
+
+def sqlConnect():
+    con = sqlite3.connect("resources/normanpd.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE incidents(incidentDate TEXT, address TEXT, incidentNumber TEXT,nature TEXT)")
+    return con
+
+def insertData(con,incidents):
+    cur = con.cursor()
+    cur.executemany("INSERT INTO incidents VALUES(?, ?, ?,?)", incidents)
+    con.commit()  # Remember to commit the transaction after executing INSERT.
+
+def getDataFromSQLite(con):
+    cur = con.cursor()
+    data = cur.execute(""" 
+        SELECT nature,COUNT(*) FROM incidents GROUP BY nature ORDER BY nature;
+""")
+    return data
+
+def convertPdfDataToJSON(pdfData):
+    reader = PdfReader(pdfData)
+    pages = reader.pages
+    incidents = []
+    for page in pages:
+        pageText = page.extract_text()
+        pageLines = pageText.splitlines()
+        for pageLine in pageLines:
+            pageLine = pageLine.replace('NORMAN POLICE DEPARTMENT','')
+            incidentDate = re.search("[0-9]+/[0-9]+/[0-9]{4} [0-9]+:[0-9]+",pageLine)
+            if incidentDate != None:
+                incidentDate = incidentDate.group()
+                pageLine = re.split(incidentDate,pageLine)[1].strip()
+                if len(pageLine)>0:
+                    incidentNumber = re.search("[0-9]{4}-[0-9]+",pageLine)
+                    incidentNumber = incidentNumber.group()
+                    pageLine = re.split(incidentNumber,pageLine)[1].strip()
+                    incidentOri = re.split(" ",pageLine)[-1]
+                    pageLine = re.split(incidentOri,pageLine)[0]
+                    # address = re.search("([A-Z]?[a-z]+/*\s*)+",pageLine)
+                    nature = re.search("(MVA)?(911)?(EMS)?\\s*[A-Z][a-z]+.*",pageLine)
+                    nature = nature.group().strip()
+                    address = re.split(nature,pageLine)[0].strip()
+                    incidents.append((incidentDate,address,incidentNumber,nature))
+    return incidents
+
+                
+def getDataFromWeb(urlString):
+    url = (urlString)
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"                          
+    data = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()  
+    memoryObject = io.BytesIO(data) 
+    return memoryObject                                                                          
+
---
+++ assignment0/main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Example main.py
+import argparse
+import functions
+
+def main(url):
+    con = functions.sqlConnect()
+    webData = functions.getDataFromWeb(url)
+    incidents = functions.convertPdfDataToJSON(webData)
+    functions.insertData(con=con,incidents=incidents)
+    data = functions.getDataFromSQLite(con)
+    total = 0
+    for unit in data:
+        total = total + unit[1]
+        print(str(unit[0])+'|'+str(unit[1]))
+    # print(total)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--incidents", type=str, required=True, 
+                         help="Incident summary url.")
+     
+    args = parser.parse_args()
+    if args.incidents:
+        main(args.incidents)
+
