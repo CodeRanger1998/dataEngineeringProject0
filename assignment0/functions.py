33439f2ddb343556e540de179a301af74f41e562
Pranil Ingle <ingle.pranil@gmail.com>
2024-02-03 19:08:56 -0500

    latest changes

--- assignment0/functions.py
+++ assignment0/functions.py
@@ -18,7 +18,7 @@ def insertData(con,incidents):
 def getDataFromSQLite(con):
     cur = con.cursor()
     data = cur.execute(""" 
-        SELECT nature,COUNT(*) FROM incidents GROUP BY nature ORDER BY COUNT(*) DESC,nature;
+        SELECT nature,COUNT(*) FROM incidents GROUP BY nature ORDER BY COUNT(*) DESC,nature DESC;
 """)
     return data
 
@@ -26,10 +26,28 @@ def convertPdfDataToJSON(pdfData):
     reader = PdfReader(pdfData)
     pages = reader.pages
     incidents = []
+    oldAddress=None
+    oldIncidentNumber = None
+    oldDate = None
+    start=False
     for page in pages:
         pageText = page.extract_text()
         pageLines = pageText.splitlines()
         for pageLine in pageLines:
+            if start:
+                # print(pageLine)
+                incidentOri = re.split(" ",pageLine)[-1]
+                pageLine = re.split(incidentOri,pageLine)[0].strip()
+                # address = re.search("([A-Z]?[a-z]+/*\s*)+",pageLine)
+                nature = re.search("(COP DDACTS)|(CIR)?(COP)?(MVA)?(911)?(EMS)?\\s*[A-Z][a-z]+.*",pageLine)
+                nature = nature.group().strip()
+                address = oldAddress + re.split(nature,pageLine)[0].strip()
+                incidents.append((oldDate,address,oldIncidentNumber,nature))
+                # print((oldDate,address,oldIncidentNumber,nature))
+                oldAddress=None
+                oldIncidentNumber = None
+                oldDate = None
+                start=False
             pageLine = pageLine.replace('NORMAN POLICE DEPARTMENT','')
             incidentDate = re.search("[0-9]+/[0-9]+/[0-9]{4} [0-9]+:[0-9]+",pageLine)
             if incidentDate != None:
@@ -40,9 +58,18 @@ def convertPdfDataToJSON(pdfData):
                     incidentNumber = incidentNumber.group()
                     pageLine = re.split(incidentNumber,pageLine)[1].strip()
                     incidentOri = re.split(" ",pageLine)[-1]
-                    pageLine = re.split(incidentOri,pageLine)[0]
+                    pageLine = re.split(incidentOri,pageLine)[0].strip()
                     # address = re.search("([A-Z]?[a-z]+/*\s*)+",pageLine)
-                    nature = re.search("(MVA)?(911)?(EMS)?\\s*[A-Z][a-z]+.*",pageLine)
+                    nature = re.search("(COP DDACTS)|(CIR)?(COP)?(MVA)?(911)?(EMS)?\\s*[A-Z][a-z]+.*",pageLine)
+                    if nature==None and len(pageLine)>0:
+                        # print(1,pageLine)
+                        oldIncidentNumber = incidentNumber
+                        oldDate = incidentDate
+                        oldAddress = pageLine + incidentOri
+                        start = True
+                        continue
+                    elif nature == None:
+                        continue
                     nature = nature.group().strip()
                     address = re.split(nature,pageLine)[0].strip()
                     incidents.append((incidentDate,address,incidentNumber,nature))
