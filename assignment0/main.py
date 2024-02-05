e5180d0051453b3ebed545d18e0b1b251124c253
Pranil Ingle <ingle.pranil@gmail.com>
2024-02-04 20:12:26 -0500

    latest changes

--- assignment0/main.py
+++ assignment0/main.py
@@ -5,15 +5,21 @@ import functions
 
 def main(url):
     con = functions.sqlConnect("normanpd.db")
-    print(url.replace('https://www.normanok.gov/sites/default/files/documents/',''))
+    # print(url.replace('https://www.normanok.gov/sites/default/files/documents/',''))
     webData = functions.getDataFromWeb(url)
     incidents = functions.convertPdfDataToJSON(webData)
     functions.insertData(con=con,incidents=incidents)
     data = functions.getDataFromSQLite(con)
     total = 0
+    emptyStringValue = ''
+    print(data)
     for unit in data:
         total = total + unit[1]
-        # print(str(unit[0])+'|'+str(unit[1]))
+        if str(unit[0]) != '':
+            print(str(unit[0])+'|'+str(unit[1]))
+        else:
+            emptyStringValue = str(unit[0])+'|'+str(unit[1])
+    print(emptyStringValue)
     # print(total)
 
 
