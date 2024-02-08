c574fd9ea551049e9e856dcabc5bcedba42174ae
Pranil Ingle <ingle.pranil@gmail.com>
2024-02-08 13:54:57 -0500

    corrections for blank natures

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='assignment0',
-	version='1.0',
+	version='2.0',
 	author='Pranil Ingle',
 	author_email='inglepranil@ufl.edu',
 	packages=find_packages(exclude=('tests', 'docs', 'resources')),
